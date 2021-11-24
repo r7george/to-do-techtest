6c44bd70453b438f4a939e44df216542bb8b00f0
Reuben George <reuben_george1998@outlook.com>
2021-11-24 15:51:58 +0000

    add-task-function-done

---
+++ assets/scripts/main.js
@@ -0,0 +1,34 @@
+const reset = document.querySelector(".nav__btn");
+const submitTask = document.querySelector(".input__btn")
+const taskDisplay = document.querySelector(".tasks__display");
+
+const taskNumbers = 0;
+
+const incrementTaskNumber = (number) => {
+  if(number == 0) {
+
+  }
+}
+
+const makeTask = (task) => {
+  return `
+  <div class="task">
+    <input class="task__checkbox" type="checkbox">
+    <p class="task__text">${task}</p>
+    <button class="task__btn"><i class="fas fa-pencil-alt"></i></button>
+  </div>
+  `
+}
+
+const handleSubmitClick = () => {
+  const input = document.querySelector(".input__task");
+  const inputVal = input.value;
+  taskDisplay.innerHTML += makeTask(inputVal);
+}
+
+const handleResetClick = () => {
+  window.location.reload();
+}
+
+reset.addEventListener("click", handleResetClick);
+submitTask.addEventListener("click", handleSubmitClick);
